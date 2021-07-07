621cfb59d4efd87a785534ed9316ecce8889d970
Eduardo Kussler <eduardoeugeniok@gmail.com>
2021-07-07 14:21:13 -0300

    Project structure created

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+function make2dArray(rows, columns) {
+  let arr = new Array(columns);
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+}
+
+let ticTacToe = (() => {
+  
+})();
